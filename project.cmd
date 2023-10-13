6b6aa345becdd9b97993fb4d7f157537354acf7d
Imre Tabur <imre.tabur@mail.ee>
2023-10-13 12:15:45 +0300

    Project deps preparation scripts

--- project.cmd
+++ project.cmd
@@ -1,2 +1,2 @@
 set SMI_PROJECT_PYTHON_VERSION=3.9
-set SMI_PROJECT_REQUIREMENTS=-r requirements.txt
+set SMI_PROJECT_REQUIREMENTS=-r requirements.txt -r requirements-ai.txt -r requirements-pytorch-ai.txt -r requirements-tensorflow-ai.txt
