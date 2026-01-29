e04aab13e82b33e7b4ee65c957905a02650fb19f
Imre Tabur <imre.tabur@mail.ee>
2026-01-29 08:27:29 +0200

    Init servivice

--- agent_start_project/agent/initialization_service.py
+++ agent_start_project/agent/initialization_service.py
@@ -11,4 +11,9 @@ class InitializationService:
     def __init__(self, file_service: FileService, tokenizer_service: TokenizerService,
                  embedding_service: EmbeddingService, tasklist_service: TasklistService, mcp_service: McpService,
                  rag_service: RagService):
-        pass
+        self.file_service = file_service
+        self.tokenizer_service = tokenizer_service
+        self.embedding_service = embedding_service
+        self.tasklist_service = tasklist_service
+        self.mcp_service = mcp_service
+        self.rag_service = rag_service
